e54cc9727646d27064573eb59066014bb7cbcd60
Cole <propertyisrobbery@hotmail.com>
2022-07-03 18:43:37 -0400

    make clear function

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 let previousExpression = document.querySelector(".previousNumber");
 console.log(previousExpression);
 let currentExpression = document.querySelector(".currentNumber");
+// want 0 to be default, and clear upon button press
+currentExpression.innerText = 0;
 //event listeners
 let buttonValue = "";
 const buttons = document.querySelectorAll("button");
@@ -9,24 +11,33 @@ buttons.forEach((button) => {
   button.addEventListener("click", () => {
     buttonValue = button.id;
     console.log(buttonValue);
+    //parseFloat(buttonValue);
+    console.log(typeof buttonValue);
     updateDisplay(buttonValue);
   });
 });
 //function to update display
-function updateDisplay(buttonValue){
+function updateDisplay(buttonValue) {
   currentExpression.append(buttonValue);
-};
-//function to clear display 
+}
+//function to clear display
 //doesn't do what I want it to
-function clear(){
-const clear =  document.querySelector(".clear");
-clear.addEventListener("click", () => {
-  buttonValue = 0;
-  updateDisplay(buttonValue);
-})
-};
-//function to delete single input
-function deleteSingle(){};
+function clear() {
+  const CLEAR = document.querySelector(".clear");
+  CLEAR.addEventListener("click", () => {
+    currentExpression.innerText = "";
+    currentExpression.innerText = "0";
+    previousExpression.innerText = "previous expression shows here";
+  });
+}
+//function to delete single input - slice isn't a function/doesn't work
+
+// const DELETE = document.querySelector(".delete");
+// DELETE.addEventListener("click", deleteSingle())
+// function deleteSingle() {
+//  let deleted = currentExpression.innerText.slice(0, -1)
+//  return deleted;
+// }
 //basic math functions:
 
 const add = function (a, b) {
@@ -66,6 +77,5 @@ const operate = function (number1, number2, operator) {
   //division
 };
 
-
-//calling functions: 
+//calling functions:
 clear();
