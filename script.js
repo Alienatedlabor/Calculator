ec93323c3c4b7ae526e341b369fd957c0b2e4650
Cole <propertyisrobbery@hotmail.com>
2022-07-05 12:59:01 -0400

    add operator display change function

--- script.js
+++ script.js
@@ -9,18 +9,16 @@ const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     buttonValue = button.id;
-    console.log(buttonValue);
-    console.log(typeof buttonValue);
     updateDisplay(buttonValue);
   });
 });
 //function to update display
 function updateDisplay(buttonValue) {
-  console.log(typeof currentExpression.innerText);
-  // replace regex gets rid of leading 0 while still allowing 0's in the currentExpression
+  // replace regex gets rid of leading 0 while still allowing 0's in the currentExpression               !!! pressing equals at 0 gives NaN
   currentExpression.innerText = currentExpression.innerText.replace(/^0+/, ``);
   currentExpression.append(buttonValue);
 }
+
 //function to clear display
 const CLEAR = document.querySelector(".clear");
 CLEAR.addEventListener("click", clear);
@@ -49,19 +47,6 @@ const subtract = function (a, b) {
   return a - b;
 };
 
-const sum = function ([...Numbers]) {
-  let total = 0;
-  for (const number of Numbers) {
-    total += number;
-  }
-  return ~~[total];
-};
-// using .reduce:
-// const sum = function(array){
-// return array.reduce((total,current) => total + current, 0)
-// }
-// 0 at the end is start total
-
 const multiply = function (array) {
   return array.reduce((total, current) => total * current);
 };
@@ -70,12 +55,42 @@ const divide = function (array) {
   return array.reduce((total, current) => total / current);
 }; //not sure if this works
 
-//function operate: takes 2 numbers and an operator and calls one of the above math functions:
-const operate = function (number1, number2, operator) {
+const operate = function (currentExpression, previousExpression, operator) {
   //addition
+  if (operator === "+") {
+    add(currentNumber, previousExpression);
+  }
   //subtraction
+  if (operator === "-") {
+    subtract(currentExpression, previousExpression);
+  }
   //multiplication
+  if (operator === "X") {
+    multiply(currentExpression, previousExpression);
+  }
   //division
+  if (operator === "/") {
+    divide(currentExpression, previousExpression);
+  }
 };
+//need to return solution upon pressing equals button:
+//function operate: takes 2 numbers and an operator and calls one of the above math functions:
+const equals = document.querySelector(".equals");
+equals.addEventListener("click", operate);
+
+//function to return result upon equals press:
+
+//adding operators to DOM, making operator selection cause display shift currentExpression to previousExpression
+const operator = document.querySelectorAll(".operator");
+operator.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    let parseNumber = parseFloat(currentExpression.innerText);
+    console.log(parseNumber);
+    console.log(typeof parseNumber);
+    previousExpression.innerText = parseNumber + operator.id;
+    currentExpression.innerText = "";
+  });
+});
+
 
 //calling functions:
